cc1627650ee2db7cdb6392f9e5c8c7f3fca0a927
Doug Brown <douglas_l_brown@apple.com>
2021-03-02 10:29:22 -0500

    Show analysis

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { Layout, Form, Input, Button, Row, Col, DatePicker, InputNumber, Select, Table } from 'antd';
+import { Layout, Form, Input, Button, Row, Col, DatePicker, InputNumber, Select, Table, Divider, Card, Statistic } from 'antd';
+import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import moment from "moment";
 import axios from "axios";
 import './App.css';
@@ -27,6 +28,7 @@ function App() {
   const [form] = Form.useForm();
   const [ledgerData, setLedgerData] = useState([]);
   const [ledgerColumns, setLedgerColumns] = useState([]);
+  const [analysisData, setAnalysisData] = useState({});
 
   useEffect(() => {
     const fetchLedger = async () => {
@@ -69,10 +71,8 @@ function App() {
         });
       });
       
-      console.log("columns", columns);
       setLedgerColumns(columns);
 
-      console.log("pre-processing options", options);
       const transformedOptions = options.map(option => {
         return {
           id: option.id,
@@ -80,14 +80,14 @@ function App() {
           status: option.status,
           contracts: option.contracts,
           strike: option.strike,
-          open_date: moment(option.open_date).format("M/DD"),          
+          open_date: moment(option.open_date).format("MM/DD"),          
           credit: option.credit,
           debit: option.debit,
-          expiration: moment(option.expiration, "M/D"),
-          close_date: moment(option.close_date, "M/D")
+          expiration: moment(option.expiration).format("MM/DD"),
+          close_date: moment(option.close_date).format("MM/DD")
         };        
       });
-      console.log("post-processing options", transformedOptions);
+      
       setLedgerData(transformedOptions);
     }
 
@@ -131,6 +131,59 @@ function App() {
     if (rsp.status === 200) {
       form.resetFields();
     } 
+  };
+
+  const onAnalysis = async () => {
+    const { contracts, credit, debit, strike, expiration, open_date} = form.getFieldsValue();
+    
+    //ROUND((((contracts * (credit - debit) * 100)) / (strike * contracts * 100)) * (365 / ((expiration::DATE - open_date::DATE))) * 100) annualized_return,
+    
+    setAnalysisData({
+      gross_credit: ((credit - debit) * 100 * contracts),
+      net_credit: ((credit - debit) * 100 * contracts) * .75,
+      collateral: strike * contracts * 100,
+      days_open: moment(expiration).diff(open_date, 'days'),
+      target_premium: strike * (.6 * moment(expiration).diff(open_date, 'days') / 365),
+      buyout_target: credit * .25,
+      daily_return: (((credit - debit) * contracts) / moment(expiration).diff(open_date, 'days')) * 100,
+      annualized_return: (((contracts * (credit - debit) * 100) / (strike * contracts * 100)) * (365 / ((moment(expiration).diff(open_date, 'days'))) * 100))
+    });
+    
+    // values.open_date = moment(values.open_date).format(dateFormat);
+    // values.expiration = moment(values.expiration).format(dateFormat);
+    // values.close_date = moment(values.close_date).format(dateFormat);
+    // values.strike = String(values.strike);
+    // values.credit = String(values.credit);
+    // values.debit = values.debit !== undefined ? String(values.debit) : undefined;
+
+    // let params = '';
+    // for (const key in values) {
+    //   if (values[key] === undefined) continue;
+      
+    //   params += `${key}: `;      
+    //   if (typeof values[key] === "string") {
+    //     params += `"${values[key]}"\n`;
+    //   } else {
+    //     params += `${values[key]}\n`;
+    //   }
+    // }
+
+    // const rsp = await fetch('http://localhost:4000/graphql', {
+    //   method: "POST",
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ query: `
+    //     mutation {
+    //       createOptionLedgerEntry(${params}) {
+    //         ticker
+    //         status
+    //       }
+    //     }`
+    //   }),
+    // });
+
+    // if (rsp.status === 200) {
+    //   form.resetFields();
+    // } 
   };
 
   const onFinishFailed = (errorInfo) => {
@@ -194,8 +247,6 @@ function App() {
                 </Select>
               </Form.Item>
               
-
-              
               <Form.Item 
                 label="Open Date"
                 name="open_date"
@@ -249,8 +300,6 @@ function App() {
                 <DatePicker format={dateFormat} onChange={onDateChange} />
               </Form.Item>
 
-              
-
               <Form.Item 
                 label="Buy Out"
                 name="debit"
@@ -260,13 +309,95 @@ function App() {
                   parser={value => value.replace(/\$\s?|(,*)/g, '')}
                 />
               </Form.Item>
-              <hr />
+              <Divider />
               <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit">
                   Submit
                 </Button>
+                <Button type="secondary" htmlType="button" onClick={onAnalysis}>
+                  Analyse
+                </Button>
               </Form.Item>
             </Form>
+            <Divider />
+            <div>
+              <Card size="small" title="Analysis Results">
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Annualized Return"
+                        value={analysisData.annualized_return}
+                        precision={2}
+                        prefix={<ArrowUpOutlined />}
+                        suffix="%"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Daily Return"
+                        value={analysisData.daily_return}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Gross Credit"
+                        value={analysisData.gross_credit}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Net Credit"
+                        value={analysisData.net_credit}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Buyout Target"
+                        value={analysisData.buyout_target}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Target Premium"
+                        value={analysisData.target_premium}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                  <Col span={12}>
+                    <Card>
+                      <Statistic
+                        title="Collateral"
+                        value={analysisData.collateral}
+                        precision={2}
+                        prefix="$"
+                      />
+                    </Card>
+                  </Col>
+                </Row>
+              </Card>
+            </div>
+            
           </Col>
         </Row>
         </Content>

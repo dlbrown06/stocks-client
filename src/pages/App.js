ef15eca4914c5f840aad22421c5c27d21ae34b80
Doug Brown <douglas_l_brown@apple.com>
2021-04-16 16:18:31 -0400

    Case insensitive username login

--- src/pages/App.js
+++ src/pages/App.js
@@ -53,7 +53,9 @@ export default function App() {
       data: JSON.stringify({
         query: `
             mutation {
-                login(email: "${values.email}", password: "${values.password}") {
+                login(email: "${values.email
+                  .toLowerCase()
+                  .trim()}", password: "${values.password.trim()}") {
                     token
                     member {
                         id
